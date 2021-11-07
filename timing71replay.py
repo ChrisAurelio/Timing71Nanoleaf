621902b039007b377c25c1623a8e4fd12a075221
ChrisAurelio <36478813+ChrisAurelio@users.noreply.github.com>
2021-11-07 01:53:35 -0400

    Add files via upload

--- timing71replay.py
+++ timing71replay.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support import wait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from nanoleafapi import Nanoleaf
+from nanoleafapi import RED, YELLOW, GREEN, WHITE
 import time
 
 options = Options()
@@ -12,11 +14,15 @@ options.add_argument("--disable-extensions")
 options.add_argument("--disable-gpu")
 #options.add_argument("--headless")
 
+nl = Nanoleaf()     # Enter Nanoleaf IP Address
+nl.set_brightness(30)
+
 def main():
     setup()
     timingLoop()
 
 def setup():
+
     print("Enter Timing71 ID: ")
     timingID = input()
     url = "https://timing71.org/replay/" + timingID
@@ -33,24 +39,24 @@ def setup():
         print("Timing71 failed to load")
 
 def timingLoop():
-
-        while (True):
-            if (driver.find_elements_by_css_selector('#app > div > div > div > div.timing-screen-header > div.flag-status.flag-status-green')):
-                print("Green")
-                time.sleep(10)
-            elif (driver.find_elements_by_css_selector('#app > div > div > div > div.timing-screen-header > div.flag-status.flag-status-caution')):
-                print("Yellow")
-                time.sleep(10)
-            elif (driver.find_elements_by_css_selector('#app > div > div > div > div.timing-screen-header > div.flag-status.flag-status-red')):
-                print("Red")
-                time.sleep(10)
-            elif (driver.find_elements_by_css_selector('#app > div > div > div > div.timing-screen-header > div.flag-status.flag-status-chequered')):
-                print("Chequered")
-                time.sleep(60)
-                quit()
-            else:
-                print("No Status")
-                time.sleep(10)
+    
+    while (True):
+        if (driver.find_elements_by_css_selector('#app > div > div > div > div.timing-screen-header > div.flag-status.flag-status-green')):
+            nl.set_color(GREEN)
+            time.sleep(10)
+        elif (driver.find_elements_by_css_selector('#app > div > div > div > div.timing-screen-header > div.flag-status.flag-status-caution')):
+            nl.set_color(YELLOW)
+            time.sleep(10)
+        elif (driver.find_elements_by_css_selector('#app > div > div > div > div.timing-screen-header > div.flag-status.flag-status-red')):
+            nl.set_color(RED)
+            time.sleep(10)
+        elif (driver.find_elements_by_css_selector('#app > div > div > div > div.timing-screen-header > div.flag-status.flag-status-chequered')):
+            nl.set_color(WHITE)
+            time.sleep(60)
+            quit()
+        else:
+            print("No Status")
+            time.sleep(10)
 
 if __name__ == "__main__":
     main()
